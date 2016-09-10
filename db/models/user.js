4567d72af5a00ab40e0c16bfccf66cb19836db05
Cuong Nguyen <cuong.nguyen@niteco.se>
2016-09-10 22:39:59 +0700

    testing online

--- db/models/user.js
+++ db/models/user.js
@@ -2,12 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var user = new Schema({
+  name: String,
+  age: Number,
   username:  String,
-  password: String,
-  clazz: {
-    type: String,
-    ref: "Class"
-  }
+  password: String
 });
 
 module.exports = mongoose.model('User', user);
