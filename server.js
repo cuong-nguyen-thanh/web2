684a8dbf10758eff05a592c5b236aa80012be7f3
Cuong Nguyen <cuong.nguyen@niteco.se>
2016-09-20 20:38:39 +0700

    đâ

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var parseurl = require('parseurl');
-
+var cookieParser = require('cookie-parser')
+var mySecret = "techkids";
 /******************************** Mongo DB ***************************/
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://admin:123456@ds033066.mlab.com:33066/techkidsweb2');
@@ -16,36 +17,38 @@ db.once('open', function() {
 
 /******************************** Express App ************************/
 var app = express();
+app.use(cookieParser('techkids', {maxAge: 120}));
+app.use(session({
+  secret: "techkids"
+}));
+
 app.use(express.static(__dirname + "/client"));
-app.use( bodyParser.json() );
+app.use(bodyParser.json() );
 
 require('./routes')(app);
 
 /******************************** Session ****************************/
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
+// app.use(function (req, res, next) {
+//   req.session.views;
+//
+//   if (!req.session.views) {
+//     req.session.views = {}
+//   }
+//
+//   // get the url pathname
+//   var pathname = parseurl(req).pathname;
+//
+//   // count the views
+//   req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+//
+//
+//   next()
+// });
+// 
+// app.get('/foo', function (req, res, next) {
+//   console.log(req.cookies);
+//   res.cookie("remember", true);
+//   res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+// });
 
-app.use(function (req, res, next) {
-  var views = req.session.views
-
-  if (!views) {
-    views = req.session.views = {}
-  }
-
-  // get the url pathname
-  var pathname = parseurl(req).pathname
-
-  // count the views
-  views[pathname] = (views[pathname] || 0) + 1
-
-  next()
-});
-
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-});
-
-http.createServer(app).listen(process.env.PORT || 5000);
+http.createServer(app).listen(process.env.PORT || 8888);
